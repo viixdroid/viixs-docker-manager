0fb87b29280ff6fbaec6e1c6fca673d46194f0a0
Ravi Rampersad <viixdroid@gmail.com>
2025-08-11 16:41:36 +0200

    Add dynamic icon creation. useful when creating icons dynamic. still need to add them to a static list, as that's needed for iconify plugin

---
+++ src/DockerManager.Shared/Models/Frontend/Icons/Iconify/Icon.cs
@@ -0,0 +1,42 @@
+﻿namespace DockerManager.Shared.Models.Frontend.Icons.Iconify;
+
+public class Icon(IconFamily iconFamily, string name, IIconType? iconType) : IIcon
+{
+    public string Name => name;
+    public IIconType? IconType => iconType;
+    public IconFamily IconFamily => iconFamily;
+
+    public string IconString
+    {
+        get
+        {
+            //TODO: Guard class
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new NotSupportedException("Icon name cannot be null or empty.");
+            }
+
+            if (IconType is null)
+            {
+                return $"icon-[{IconFamily}--{Name}]";
+            }
+
+            return $"icon-[{IconFamily}--{Name}-{IconType}]";
+        }
+    }
+
+    public static Icon SolarLineDuotone(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.LineDuotone);
+    public static Icon SolarBold(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.Bold);
+    public static Icon SolarBroken(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.Broken);
+    public static Icon SolarLinear(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.Linear);
+    public static Icon SolarOutline(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.Outline);
+    public static Icon SolarBoldDuotone(string name) => new Icon(IconFamily.Solar, name, SolarIconTypeV2.BoldDuotone);
+
+    public static Icon MaterialDesignLight(string name) => new Icon(IconFamily.MdiLight, name, null);
+    public static Icon MaterialDesign(string name) => new Icon(IconFamily.Mdi, name, null);
+
+    public static Icon FluentRegular(string name) => new Icon(IconFamily.Fluent, name, FluentIconTypeV2.Regular);
+    public static Icon FluentFilled(string name) => new Icon(IconFamily.Fluent, name, FluentIconTypeV2.Filled);
+    public static Icon FluentLight(string name) => new Icon(IconFamily.Fluent, name, FluentIconTypeV2.Light);
+    public static Icon FluentColor(string name) => new Icon(IconFamily.FluentColor, name, FluentIconTypeV2.Color);
+}
---
+++ src/DockerManager/Constants/IconSafeList.cs
@@ -0,0 +1,16 @@
+﻿// This file is not meant to be compiled or used directly in the application.
+// It serves as a safelist for the Tailwind CSS build process to ensure
+// dynamically used icon classes are generated.
+
+/*
+    icon-[mdi--home]
+    icon-[mdi-light--cog]
+    icon-[solar--info-circle-bold-duotone]
+    icon-[mdi--delete]
+    icon-[mdi--pencil]
+    icon-[mdi--plus]
+    icon-[mdi--server]
+    icon-[mdi--settings]
+    // Add any other icons you use dynamically in your Blazor components here.
+*/
+
