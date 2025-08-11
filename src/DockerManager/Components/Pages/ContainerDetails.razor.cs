d46b988d399be161f1a5bebb96a9182195775016
Ravi Rampersad <viixdroid@gmail.com>
2025-08-11 18:08:35 +0200

    Add first version of container details view

---
+++ src/DockerManager/Components/Pages/ContainerDetails.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DockerManager.Components.Pages;
+
+public partial class ContainerDetails : ComponentBase
+{
+    [Parameter] 
+    public string ContainerName { get; set; }
+}
