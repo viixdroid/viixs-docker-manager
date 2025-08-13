784dd0ead3cdfb5249eba735304a965fef11d8bf
Ravi Rampersad <viixdroid@gmail.com>
2025-08-13 14:50:41 +0200

    List containers

--- src/DockerManager/Components/Pages/Containers.razor.cs
+++ src/DockerManager/Components/Pages/Containers.razor.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using DockerManager.Shared.Models.Docker;
+using DockerManager.Shared.Services.Docker.Interfaces;
+using Microsoft.AspNetCore.Components;
 
 namespace DockerManager.Components.Pages;
 
-public partial class Containers : ComponentBase
+public partial class Containers(IDockerService dockerService) : ComponentBase
 {
+    private IEnumerable<ContainerSummary> _containerSummaries = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        _containerSummaries = await dockerService.GetContainerListAsync();
+    }
 }
