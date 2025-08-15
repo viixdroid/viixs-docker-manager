7fc2c76b3ecb1e7e5a86914b27e6b9f030e5f05d
Ravi Rampersad <viixdroid@gmail.com>
2025-08-15 13:18:13 +0200

    Allow the frontend to login, using authentication providers

---
+++ src/DockerManager/Providers/CustomAuthenticationProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+using DockerManager.Auth.Models.Responses;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+using static DockerManager.Constants.ApplicationConstants.Authentication;
+
+namespace DockerManager.Providers;
+
+public class CustomAuthenticationProvider(ProtectedLocalStorage protectedLocalStorage) : AuthenticationStateProvider
+{
+    private readonly ClaimsPrincipal _anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var getTokenResult = await protectedLocalStorage.GetAsync<UserLoggedInModel>(TokenStorageKey);
+        if (!getTokenResult.Success || getTokenResult.Value is null)
+        {
+            return new AuthenticationState(_anonymousUser);
+        }
+
+        var userLoggedInModel = getTokenResult.Value;
+        if (userLoggedInModel is null)
+        {
+            return new AuthenticationState(_anonymousUser);
+        }
+
+        var claims = GetClaimsFromJsonWebToken(userLoggedInModel.Token).ToList();
+        if (claims.Count == 0)
+        {
+            return new AuthenticationState(_anonymousUser);
+        }
+        var identity = new ClaimsIdentity(claims, JsonWebTokenAuthenticationType);
+        var user = new ClaimsPrincipal(identity);
+        return new AuthenticationState(user);
+    }
+
+    // TODO: move to a more appropriate class
+    public void AuthenticateUser(string token)
+    {
+        var claims = GetClaimsFromJsonWebToken(token).ToList();
+        var identity = new ClaimsIdentity(claims, JsonWebTokenAuthenticationType);
+        var user = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    // TODO: move to a more appropriate class
+    private static IEnumerable<Claim> GetClaimsFromJsonWebToken(string token)
+    {
+        var tokenHandler = new JsonWebTokenHandler();
+        if (!tokenHandler.CanReadToken(token))
+        {
+            return [];
+        }
+
+        var readToken = tokenHandler.ReadJsonWebToken(token);
+
+        return readToken.Claims;
+    }
+}
---
+++ src/DockerManager/Services/ILoginService.cs
@@ -0,0 +1,8 @@
+﻿using DockerManager.Auth.Models.Pages;
+
+namespace DockerManager.Services;
+
+public interface ILoginService
+{
+    Task<bool> LoginAsync(LoginModel loginModel);
+}
