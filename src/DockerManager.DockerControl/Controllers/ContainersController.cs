daeda64eab1b947631ffcc64b3aa05a392813e0b
Ravi Rampersad <viixdroid@gmail.com>
2025-08-19 18:48:17 +0200

    Add controllers and services for getting container data

---
+++ src/DockerManager.DockerControl/Controllers/ContainersController.cs
@@ -0,0 +1,20 @@
+﻿using DockerManager.DockerControl.Models;
+using DockerManager.DockerControl.Services.Interfaces;
+using DockerManager.Shared.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerManager.DockerControl.Controllers;
+
+[ApiController]
+[Route("api/Docker/[controller]")]
+// [Authorize]
+public class ContainersController(IDockerService dockerService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetContainersAsync()
+    {
+        var result = await dockerService.GetContainerListAsync();
+        return Ok(ResponseObject<IEnumerable<ContainerSummary>>.Success(result));
+    }
+}
