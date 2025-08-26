a2d77e0b3d68b701d7b74fc65f4d3fc45cf0da51
Ravi Rampersad <viixdroid@gmail.com>
2025-08-26 19:25:14 +0200

    remove not needed constraint.

--- src/ViixsDockerManager/ViixsDockerManager.Shared/Helpers/Guard.cs
+++ src/ViixsDockerManager/ViixsDockerManager.Shared/Helpers/Guard.cs
@@ -5,7 +5,6 @@ namespace ViixsDockerManager.Shared.Helpers;
 public static class Guard
 {
     public static TValue ValueIsNotNull<TValue>(TValue? value, string parameterName)
-        where TValue : class
     {
         ViixsDockerManagerException.ThrowIfArgumentNull(value, parameterName);
         return value!;
