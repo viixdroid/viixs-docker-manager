e4ff3a66c906dde14e2e423b9ca30b6dfaf72925
Ravi Rampersad <viixdroid@gmail.com>
2025-08-26 19:24:43 +0200

    add cache for constructor

---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared/Helpers/ConstructorCache.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using ViixsDockerManager.Shared.Exceptions;
+using ViixsDockerManager.Shared.Helpers.Models;
+
+namespace ViixsDockerManager.Shared.Helpers;
+
+internal static class ConstructorCache
+{
+    private static readonly ConcurrentDictionary<Type, ConstructorMetadata> _typeConstructorCache =
+        new ConcurrentDictionary<Type, ConstructorMetadata>();
+
+    public static ConstructorMetadata GetOrAddConstructorMetadata<TImplementationType>()
+        => GetOrAddConstructorMetadata(typeof(TImplementationType));
+
+    private static ConstructorMetadata GetOrAddConstructorMetadata(Type implementationType)
+    {
+        return _typeConstructorCache.GetOrAdd(implementationType, type =>
+        {
+            var constructors = implementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            var firstConstructor = CheckAndGetConstructor(constructors, type.Name);
+
+            return new ConstructorMetadata(firstConstructor, firstConstructor.GetParameters());
+        });
+    }
+
+    private static ConstructorInfo CheckAndGetConstructor(ConstructorInfo[] constructors, string typeName)
+    {
+        switch (constructors)
+        {
+            case { Length: 0 }:
+                throw ViixsDockerManagerException.InvalidOperation(
+                    $"Cannot register {typeName}: No public constructor found.");
+            case { Length: > 1 }:
+                throw ViixsDockerManagerException.InvalidOperation(
+                    $"Cannot register {typeName}: Only a single public constructor is allowed for services in the ServiceProvider.");
+        }
+
+        return constructors[0];
+    }
+}
---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared/Helpers/Models/ConstructorMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using ViixsDockerManager.Shared.Exceptions;
+
+namespace ViixsDockerManager.Shared.Helpers.Models;
+
+internal sealed record ConstructorMetadata(ConstructorInfo ConstructorInfo, ParameterInfo[] Parameters)
+{
+    public TResult InvokeConstructor<TResult>(IServiceProvider serviceProvider)
+    {
+        if (typeof(TResult) != ConstructorInfo.DeclaringType)
+        {
+            ViixsDockerManagerException.InvalidOperation(
+                "Trying to construct a object that is not for the requested type");
+        }
+
+        var parameters = ResolveParameters(serviceProvider);
+        var result = ConstructorInfo.Invoke(parameters.ToArray());
+        return (TResult)result;
+    }
+
+    private IEnumerable<object?> ResolveParameters(IServiceProvider serviceProvider)
+    {
+        return Parameters.Select(p => serviceProvider.GetService(p.ParameterType));
+    }
+}
