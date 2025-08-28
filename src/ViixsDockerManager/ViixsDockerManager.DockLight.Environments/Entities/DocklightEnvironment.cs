0a7010b3634eb53968b30f1cccf1e882448e22fd
Ravi Rampersad <viixdroid@gmail.com>
2025-08-28 20:21:41 +0200

    Add project for environment objects

---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/Entities/DocklightEnvironment.cs
@@ -0,0 +1,21 @@
+﻿namespace ViixsDockerManager.DockLight.Environments.Entities;
+
+public class DocklightEnvironment
+{
+    /// <summary>
+    /// The id of this database object
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// The name of this database object
+    /// </summary>
+    public required string Name { get; set; }
+    /// <summary>
+    /// The location where the Environment can be found.
+    ///
+    /// For local connections this will /var/docker/docker.sock
+    ///
+    /// for external connections, this can be an ip address.
+    /// </summary>
+    public required string ApiLocation { get; set; }
+}
---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/readme.md
@@ -0,0 +1,3 @@
+﻿# ViixsDockerManager.DockLight.Environments
+
+This project contains everything needed to create a new environment, on to which we want to connect and show things like the containers for a specific environment
