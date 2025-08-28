fc1ab011f37068c792d03f9bb42d5c6089f74cb8
Ravi Rampersad <viixdroid@gmail.com>
2025-08-28 12:42:44 +0200

    Interface responseobject

---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared/Models/IResponseObject.cs
@@ -0,0 +1,12 @@
+﻿namespace ViixsDockerManager.Shared.Models;
+
+public interface IResponseObject
+{
+    bool IsSuccess { get; }
+    IEnumerable<string>? Errors { get; }
+}
+
+public interface IResponseObject<out TResult> : IResponseObject
+{
+    TResult Result { get; }
+}
