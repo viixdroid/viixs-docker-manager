356be60e7ef32ffeebc9fa6ee05c713e5406dec8
Ravi Rampersad <viixdroid@gmail.com>
2025-09-01 19:58:49 +0200

    Add environment id

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/DocklightEnvironmentReadDbContext.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/DocklightEnvironmentReadDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using ViixsDockerManager.DockLight.Environments.Entities;
+using ViixsDockerManager.DockLight.Shared.Entities;
 using ViixsDockerManager.Shared.Database.Contexts;
 
 namespace ViixsDockerManager.DockLight.Environments;
@@ -14,12 +14,14 @@ public class DocklightEnvironmentReadDbContext(DbContextOptions options) : BaseR
         modelBuilder.Entity<DocklightEnvironment>(docklightEnvironment =>
         {
             docklightEnvironment.HasKey(dle => dle.Id);
+            docklightEnvironment.HasAlternateKey(dle => dle.EnvironmentId);
+            docklightEnvironment.Property(dle => dle.EnvironmentId).ValueGeneratedOnAdd();
             docklightEnvironment.Property(dle => dle.Id).ValueGeneratedOnAdd();
             //TODO: Remove when added write functionality
             docklightEnvironment.HasData(
-                new DocklightEnvironment() { Id = 1, Name = "Local", ApiLocation = "/var/docker/docker.sock" },
-                new DocklightEnvironment() { Id = 2, Name = "Nas", ApiLocation = "http://nas" },
-                new DocklightEnvironment() { Id = 3, Name = "Nuc", ApiLocation = "http://nuc" }
+                new DocklightEnvironment() { Id = 1, EnvironmentId = Guid.Parse("34805C86-9086-45E1-B264-241983ACC044"), Name = "Local", ApiLocation = "/var/docker/docker.sock" },
+                new DocklightEnvironment() { Id = 2, EnvironmentId = Guid.Parse("1ACF8E3C-495A-4258-A280-CC7ACF504C0E"), Name = "Nas", ApiLocation = "http://nas" },
+                new DocklightEnvironment() { Id = 3, EnvironmentId = Guid.Parse("4D9E6256-D601-41F4-ABE6-A42F40100008"), Name = "Nuc", ApiLocation = "http://nuc" }
             );
         });
     }
