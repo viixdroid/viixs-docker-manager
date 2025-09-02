47f445cabc5fcfc7540b8673025cad38465e0227
Ravi Rampersad <viixdroid@gmail.com>
2025-09-02 02:12:44 +0200

    Add Migrations

---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/Migrations/20250902000406_AddWindowsLocation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ViixsDockerManager.DockLight.Environments.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWindowsLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.InsertData(
+                table: "DocklightEnvironment",
+                columns: new[] { "Id", "ApiLocation", "EnvironmentId", "Name" },
+                values: new object[,]
+                {
+                    { 2, "\\pipe\\docker_engine", new Guid("65670bc8-fb6e-413a-9492-3ba24fd4f8ff"), "LocalWindows" },
+                    { 3, "http://nas", new Guid("1acf8e3c-495a-4258-a280-cc7acf504c0e"), "Nas" },
+                    { 4, "http://nuc", new Guid("4d9e6256-d601-41f4-abe6-a42f40100008"), "Nuc" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.InsertData(
+                table: "DocklightEnvironment",
+                columns: new[] { "Id", "ApiLocation", "EnvironmentId", "Name" },
+                values: new object[,]
+                {
+                    { 2, "http://nas", new Guid("1acf8e3c-495a-4258-a280-cc7acf504c0e"), "Nas" },
+                    { 3, "http://nuc", new Guid("4d9e6256-d601-41f4-abe6-a42f40100008"), "Nuc" }
+                });
+        }
+    }
+}
---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/Migrations/20250902000641_UpdateWindowsLocation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViixsDockerManager.DockLight.Environments.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWindowsLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ApiLocation",
+                value: "/pipe/docker_engine");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DocklightEnvironment",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ApiLocation",
+                value: "\\pipe\\docker_engine");
+        }
+    }
+}
