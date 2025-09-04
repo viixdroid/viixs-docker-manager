18ac79a55a3617e2477b3c078465fa63493f4511
Ravi Rampersad <viixdroid@gmail.com>
2025-09-04 17:55:21 +0200

    add write entity services

--- src/ViixsDockerManager/ViixsDockerManager.Shared.Database/Extensions/SharedDatabaseServiceExtensions.cs
+++ src/ViixsDockerManager/ViixsDockerManager.Shared.Database/Extensions/SharedDatabaseServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ViixsDockerManager.Shared.Database.Contexts;
 using ViixsDockerManager.Shared.Database.Entities;
 using ViixsDockerManager.Shared.Database.Repositories;
 using ViixsDockerManager.Shared.Database.UnitOfWorks;
@@ -8,13 +9,22 @@ namespace ViixsDockerManager.Shared.Database.Extensions;
 
 public static class SharedDatabaseServiceExtensions
 {
-    public static IServiceCollection AddEntityServices<TReadDbContext, TEntity>(this IServiceCollection services)
-        where TReadDbContext : DbContext
+    public static IServiceCollection AddReadEntityServices<TReadDbContext, TEntity>(this IServiceCollection services)
+        where TReadDbContext : DbContext, IReadDbContext
         where TEntity : class, IEntity
     {
         services
             .AddScoped<IReadUnitOfWork<TEntity>, ReadUnitOfWork<TEntity>>()
             .AddScoped<IReadRepository<TEntity>, ReadRepository<TReadDbContext, TEntity>>();
+        return services;
+    }
+
+    public static IServiceCollection AddWriteEntityServices<TWriteDbContext, TEntity>(this IServiceCollection services)
+        where TWriteDbContext : DbContext, IWriteDbContext
+        where TEntity : class, IEntity
+    {
+        services
+            .AddScoped<IDatabaseWriteRepository<TEntity>, DatabaseWriteRepository<TWriteDbContext, TEntity>>();
         return services;
     }
 }
