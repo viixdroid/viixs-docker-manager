dab310cf757ca94337936298c5814fdf124ad803
Ravi Rampersad <viixdroid@gmail.com>
2025-09-10 01:24:32 +0200

    Add Card for iniital setup

---
+++ src/ViixsDockerManager/viixsdockermanager.client/src/components/setup/StepperCard.tsx
@@ -0,0 +1,24 @@
+﻿import {Card, CardContent, Step, StepLabel, Stepper} from "@mui/material";
+
+interface StepperProps {
+  activeStep: number;
+  steps: string[]// for now string array. should move to object based steps
+}
+
+const StepperCard = ({activeStep, steps}: StepperProps) => {
+  return (
+    <Card sx={{margin: 'auto', mt: 2, p: 2}}>
+      <CardContent>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default StepperCard
