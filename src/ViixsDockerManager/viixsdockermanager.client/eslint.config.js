42e8e85dada7cfd3f904d15849fa35795d4d8beb
Ravi Rampersad <viixdroid@gmail.com>
2025-09-10 18:47:36 +0200

    use antfu eslint config

--- src/ViixsDockerManager/viixsdockermanager.client/eslint.config.js
+++ src/ViixsDockerManager/viixsdockermanager.client/eslint.config.js
@@ -1,36 +1,3 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-import stylistic from "@stylistic/eslint-plugin";
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
+import antfu from '@antfu/eslint-config'
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    plugins: {
-      stylistic,
-    },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.strictTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs["recommended-latest"],
-      reactX.configs['recommended-typescript'],
-      reactRefresh.configs.vite,
-      reactDom.configs.recommended,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      "@stylistic/indent": [error, 2],
-    },
-  },
-]);
+export default antfu({})
