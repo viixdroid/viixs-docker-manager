3dae697b3d14a06b6b7dd9c5a416a47f99522a40
Ravi Rampersad <viixdroid@gmail.com>
2025-09-10 15:25:22 +0200

    update linter rules

--- src/ViixsDockerManager/viixsdockermanager.client/eslint.config.js
+++ src/ViixsDockerManager/viixsdockermanager.client/eslint.config.js
@@ -1,23 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      stylistic,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
+      reactX.configs['recommended-typescript'],
       reactRefresh.configs.vite,
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "@stylistic/indent": [error, 2],
+    },
   },
-])
+]);
