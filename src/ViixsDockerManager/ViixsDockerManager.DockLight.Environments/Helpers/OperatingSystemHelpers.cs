aeaf180612ed57e74cffd3ba1dee63f41f3fc13a
Ravi Rampersad <viixdroid@gmail.com>
2025-09-11 16:01:48 +0200

    Add OS helper for getting better platformnames (Windows, Linux, macOS etc.)

---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Environments/Helpers/OperatingSystemHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ViixsDockerManager.DockLight.Environments.Helpers;
+
+internal static class OperatingSystemHelpers
+{
+    private static bool IsWindows()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    private static bool IsLinux()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+    private static bool IsMacOS()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+    public static string GetPlatformName()
+    {
+        const string linux = "Linux";
+        const string windows = "Windows";
+        const string macOS = "macOS";
+
+        if (IsWindows())
+        {
+            return windows;
+        }
+        if (IsLinux())
+        {
+            return linux;
+        }
+        if (IsMacOS())
+        {
+            return macOS;
+        }
+        return "Unknown";
+    }
+}
