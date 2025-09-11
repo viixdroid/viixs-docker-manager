c5c900f912f0a326ccf0fd49664dafe329955c7a
Ravi Rampersad <viixdroid@gmail.com>
2025-09-11 19:32:43 +0200

    add extra json options for serializing

--- src/ViixsDockerManager/ViixsDockerManager.Server/Program.cs
+++ src/ViixsDockerManager/ViixsDockerManager.Server/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using ViixDockerManager.AspNet.Shared.Extensions;
@@ -38,7 +39,7 @@ builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddMediatorServices();
-builder.Services.AddDockLightServices();
+builder.Services.AddDockLightContainerServices();
 builder.Services.AddDockLightEnvironmentServices(builder.Configuration);
 builder.Services.AddDockLightSharedServices();
 builder.Services.AddExceptionHandlerService();
@@ -50,7 +51,10 @@ builder.Services.AddControllers(options =>
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 var app = builder.Build();
 
