fd5f9556f032e40f4b2d53c15e2f0b644bf4a1bb
Ravi Rampersad <viixdroid@gmail.com>
2025-09-12 16:48:02 +0200

    rename to factory, as it;s a factory.

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/Interfaces/IDockerServiceFactory.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/Interfaces/IDockerServiceFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace ViixsDockerManager.DockLight.Services.Interfaces;
 
-internal interface IDockerServiceRunner
+internal interface IDockerServiceFactory
 {
     Task<TRequestedService> GetServiceAsync<TRequestedService>(Guid environmentId) where TRequestedService : class, IDockerService;
 }
