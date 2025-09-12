6a181a27038fe2de2ffa1ed8562365b5897afb94
Ravi Rampersad <viixdroid@gmail.com>
2025-09-12 16:52:21 +0200

    add empty services and interfaces for images and system

---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/DockerImageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using ViixsDockerManager.DockLight.Services.Interfaces;
+
+namespace ViixsDockerManager.DockLight.Services;
+
+internal class DockerImageService : IDockerImageService
+{
+    private IImageOperations _images;
+
+    public DockerImageService(IImageOperations images) => _images = images;
+
+    public Task DeleteImageAsync(string imageId, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+    public Task<string> GetImageDetailsAsync(string imageId, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+    public Task<IEnumerable<string>> GetImageListAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+}
---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/Interfaces/IDockerSystemService.cs
@@ -0,0 +1,8 @@
+﻿namespace ViixsDockerManager.DockLight.Services.Interfaces;
+
+internal interface IDockerSystemService : IDockerService
+{
+    public Task<string> GetSystemInfoAsync(CancellationToken cancellationToken = default);
+
+    public Task<string> GetDockerVersionAsync(CancellationToken cancellationToken = default);
+}
