7388bd2876acf57f24eae8b0440053e1687f799c
Ravi Rampersad <viixdroid@gmail.com>
2025-09-12 10:34:48 +0200

    remove forgotten usings

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Extensions/DockLightServicesExtensions.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Extensions/DockLightServicesExtensions.cs
@@ -1,20 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ViixsDockerManager.DockLight.Controllers;
 using ViixsDockerManager.DockLight.Handlers;
 using ViixsDockerManager.DockLight.Models.Queries;
-using ViixsDockerManager.DockLight.Services;
 using ViixsDockerManager.DockLight.Services.Interfaces;
 using ViixsDockerManager.DockLight.Services.Runners;
-using ViixsDockerManager.DockLight.Shared.Decorators;
-using ViixsDockerManager.DockLight.Shared.Decorators.Providers;
-using ViixsDockerManager.DockLight.Shared.Entities;
 using ViixsDockerManager.DockLight.Shared.Services;
 using ViixsDockerManager.DockLight.Shared.Services.Interfaces;
 using ViixsDockerManager.Mediator.Extensions;
-using ViixsDockerManager.Shared.Database.Repositories;
 using ViixsDockerManager.Shared.Extensions;
 
 namespace ViixsDockerManager.DockLight.Extensions;
@@ -28,12 +22,6 @@ public static class DockLightServicesExtensions
 
         services.RegisterQueryHandler<GetContainersForEnvironmentHandler, GetContainersForEnvironmentQuery>();
         services.RegisterQueryHandler<GetContainerDetailsHandler, GetContainerDetailsQuery>();
-
-        //services.AddDecoration<IDockerContainersOldService, DockerContainersService>((serviceToDecorate, serviceProvider) => DockerClientDecorator<IDockerContainersOldService>
-        //    .CreateService(
-        //        serviceToDecorate,
-        //        serviceProvider.GetRequiredService<IDockerClientEndpointProvider>())
-        //);
 
         return services;
     }
