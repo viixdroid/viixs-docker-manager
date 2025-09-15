423e71654aabae633b5ba19c346fb53b820446fd
Ravi Rampersad <viixdroid@gmail.com>
2025-09-15 16:56:53 +0200

    add a provider for signalr. This makes server->client communcation possible and gives a better experience for when start or stop is pressed.

---
+++ src/ViixsDockerManager/viixsdockermanager.client/src/components/signalr/DockLightHubProvider.tsx
@@ -0,0 +1,47 @@
+import type { FC, ReactNode } from 'react'
+import * as signalr from '@microsoft/signalr'
+import { createContext, useContext, useState } from 'react'
+
+interface DockLightHubContextType {
+  connection: signalr.HubConnection | null
+  connect: (environmentId: string) => Promise<void>
+}
+
+const DockLightHubContext = createContext<DockLightHubContextType | undefined>(undefined)
+
+interface DockLightHubProviderProps {
+  children: ReactNode
+}
+
+export const DockLightHubProvider: FC<DockLightHubProviderProps> = ({ children }: DockLightHubProviderProps) => {
+  const [connection, setConnection] = useState<signalr.HubConnection | null>(null)
+
+  const connect = async (environmentId: string) => {
+    if (connection) {
+      void connection.stop()
+    }
+
+    const newConnection = new signalr.HubConnectionBuilder()
+      .withUrl(`/ws/docklight?environmentId=${environmentId}`)
+      .withAutomaticReconnect()
+      .build()
+
+    await newConnection?.start()
+
+    setConnection(newConnection)
+  }
+
+  return (
+    <DockLightHubContext.Provider value={{ connection, connect }}>
+      {children}
+    </DockLightHubContext.Provider>
+  )
+}
+
+export function useDockLightHub(): DockLightHubContextType {
+  const context = useContext(DockLightHubContext)
+  if (!context) {
+    throw new Error('useDockLightHub must be used within a DockLightHubProvider')
+  }
+  return context
+}
