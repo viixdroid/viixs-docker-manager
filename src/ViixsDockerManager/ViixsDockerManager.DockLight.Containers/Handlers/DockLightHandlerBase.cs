62c4244cf53be173fcfa9b83db852dd85324a92e
Ravi Rampersad <viixdroid@gmail.com>
2025-09-15 17:04:09 +0200

    update handler base to use interface and not class

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Handlers/DockLightHandlerBase.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Handlers/DockLightHandlerBase.cs
@@ -1,16 +1,17 @@
-﻿using ViixsDockerManager.DockLight.Models.Queries;
+﻿using ViixsDockerManager.DockLight.Models;
+using ViixsDockerManager.DockLight.Models.Queries;
 using ViixsDockerManager.DockLight.Services.Interfaces;
 
 namespace ViixsDockerManager.DockLight.Handlers;
 
 internal abstract class DockLightHandlerBase(IDockerServiceFactory dockerServiceFactory)
 {
-    protected Task<IDockerContainerService> GetDockerContainerService(DockerBaseQuery dockerBaseQuery, CancellationToken cancellationToken = default)
-        => dockerServiceFactory.GetServiceAsync<IDockerContainerService>(dockerBaseQuery.EnvironmentId);
+    protected Task<IDockerContainerService> GetDockerContainerService(IEnvironmentContext envivronmentContext, CancellationToken cancellationToken = default)
+        => dockerServiceFactory.GetServiceAsync<IDockerContainerService>(envivronmentContext.EnvironmentId);
 
-    protected Task<IDockerImageService> GetDockerImageService(DockerBaseQuery dockerBaseQuery, CancellationToken cancellationToken = default)
-        => dockerServiceFactory.GetServiceAsync<IDockerImageService>(dockerBaseQuery.EnvironmentId);
+    protected Task<IDockerImageService> GetDockerImageService(IEnvironmentContext envivronmentContext, CancellationToken cancellationToken = default)
+        => dockerServiceFactory.GetServiceAsync<IDockerImageService>(envivronmentContext.EnvironmentId);
 
-    protected Task<IDockerSystemService> GetDockerSystemService(DockerBaseQuery dockerBaseQuery, CancellationToken cancellationToken = default)
-        => dockerServiceFactory.GetServiceAsync<IDockerSystemService>(dockerBaseQuery.EnvironmentId);
+    protected Task<IDockerSystemService> GetDockerSystemService(IEnvironmentContext envivronmentContext, CancellationToken cancellationToken = default)
+        => dockerServiceFactory.GetServiceAsync<IDockerSystemService>(envivronmentContext.EnvironmentId);
 }
