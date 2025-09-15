c1d2e384708ddeeb6498e9d5fc35a86a4fd7c2bc
Ravi Rampersad <viixdroid@gmail.com>
2025-09-15 17:02:24 +0200

    Add strongly type signalr hub. Saves magic strings :)

---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Controllers/Hubs/DockLightInformationHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ViixsDockerManager.DockLight.Exceptions;
+
+namespace ViixsDockerManager.DockLight.Controllers.Hubs;
+
+internal class DockLightInformationHub : Hub<IDockLightInformationContext>
+{
+    private const string QueryStringEnvironmentIdKey = "environmentId";
+
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var environmentIdString = httpContext?.Request.Query[QueryStringEnvironmentIdKey].ToString();
+
+        if (!Guid.TryParse(environmentIdString, out var environmentId))
+        {
+            throw new ConnectionHubInvalidEnvironmentId(environmentIdString!);
+        }
+
+        var groupName = environmentId.ToString();
+        await AddToGroup(groupName).ConfigureAwait(false);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var httpContext = Context.GetHttpContext();
+        var environmentIdString = httpContext?.Request.Query[QueryStringEnvironmentIdKey].ToString();
+        if (Guid.TryParse(environmentIdString, out var environmentId))
+        {
+            var groupName = environmentId.ToString();
+            await RemoveFromGroup(groupName).ConfigureAwait(false);
+        }
+        await base.OnDisconnectedAsync(exception).ConfigureAwait(false);
+    }
+
+
+    private async Task AddToGroup(string environmentId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, environmentId).ConfigureAwait(false);
+    }
+
+    private async Task RemoveFromGroup(string environmentId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, environmentId).ConfigureAwait(false);
+    }
+}
+
---
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Controllers/Hubs/IDockLightInformationContext.cs
@@ -0,0 +1,9 @@
+﻿namespace ViixsDockerManager.DockLight.Controllers.Hubs;
+
+public interface IDockLightInformationContext
+{
+    Task OnContainerStarted(string containerId, bool isSuccessfullyStarted);
+    Task OnContainerStopped(string containerId);
+    Task OnContainerRestarted(string containerId);
+    Task OnContainerKilled(string containerId);
+}
