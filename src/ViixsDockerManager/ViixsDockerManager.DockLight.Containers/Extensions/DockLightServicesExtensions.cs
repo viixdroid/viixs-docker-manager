c2534a1d2eb3777af4f6aabe9b75b9bda0906964
Ravi Rampersad <viixdroid@gmail.com>
2025-09-15 17:03:45 +0200

    add and register extra mappings

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Extensions/DockLightServicesExtensions.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Extensions/DockLightServicesExtensions.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using ViixsDockerManager.DockLight.Controllers;
+using ViixsDockerManager.DockLight.Controllers.Hubs;
 using ViixsDockerManager.DockLight.Handlers;
+using ViixsDockerManager.DockLight.Models.Commands;
 using ViixsDockerManager.DockLight.Models.Queries;
+using ViixsDockerManager.DockLight.Services;
 using ViixsDockerManager.DockLight.Services.Interfaces;
 using ViixsDockerManager.DockLight.Services.Runners;
 using ViixsDockerManager.DockLight.Shared.Services;
@@ -18,19 +21,34 @@ public static class DockLightServicesExtensions
     public static IServiceCollection AddDockLightContainerServices(this IServiceCollection services)
     {
         services.AddDecoration<IDockerClientService, DockerClientService>();
+        services.AddDecoration<ISendContainerNotifications, SendContainerNotifications>();
         services.AddScoped<IDockerServiceFactory, DockerServiceFactory>();
 
         services.RegisterQueryHandler<GetContainersForEnvironmentHandler, GetContainersForEnvironmentQuery>();
         services.RegisterQueryHandler<GetContainerDetailsHandler, GetContainerDetailsQuery>();
+        services.RegisterCommandHandler<StartContainerHandler, StartContainerCommand>();
 
         return services;
     }
 
-    public static RouteGroupBuilder MapDocklightRoutes(this IEndpointRouteBuilder serviceHost)
+    public static RouteGroupBuilder MapDocklightRoutes(this IEndpointRouteBuilder endPointRouteBuilder)
     {
-        var group = serviceHost.MapGroup("{environmentId:guid}/containers");
+        var group = endPointRouteBuilder.MapGroup("{environmentId:guid}/containers");
 
-        group.MapDocklightRouteActions();
+        var dockLightRoutesGroup = group.MapDocklightRouteActions();
+        MapDockLightContainerRoutes(dockLightRoutesGroup);
         return group;
+    }
+
+    private static RouteGroupBuilder MapDockLightContainerRoutes(this IEndpointRouteBuilder endPointRouteBuilder)
+    {
+        var group = endPointRouteBuilder.MapGroup("/{containerId}");
+        group.MapDockLightContainerRouteActions();
+        return group;
+    }
+
+    public static void MapDockLightSignalRHubs(this IEndpointRouteBuilder endPointRouteBuilder)
+    {
+        endPointRouteBuilder.MapHub<DockLightInformationHub>("/docklight");
     }
 }
