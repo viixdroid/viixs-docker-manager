aa4ca97ae2d9c19fb3161e8cc995e62d5ef85cb4
Ravi Rampersad <viixdroid@gmail.com>
2025-09-16 18:48:00 +0200

    Add services to get data, also for actions

---
+++ src/ViixsDockerManager/viixsdockermanager.client/src/services/DockLightEnvironmentService.ts
@@ -0,0 +1,12 @@
+import type { DockLightEnvironment } from '../models/dock-light-environment'
+import type { CreateDockLightEnvironmentCommand } from '../pages/setup/(models)/initialdocklightenvironment'
+import backendApi from '../clients/BackendApiClient'
+
+const Url = 'docklightenvironments'
+
+const DockLightEnvironmentService = {
+  getAllDockLightEnvironments: async (): Promise<DockLightEnvironment[]> => backendApi.get(Url),
+  createDockLightEnvironment: async (createDockLightEnvironmentCommand: CreateDockLightEnvironmentCommand): Promise<void> => backendApi.post(Url, { json: createDockLightEnvironmentCommand }),
+}
+
+export default DockLightEnvironmentService
