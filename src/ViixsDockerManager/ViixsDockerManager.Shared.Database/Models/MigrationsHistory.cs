35b5ed7a1006ec508f2ca24bc72186cb650b9c62
Ravi Rampersad <viixdroid@gmail.com>
2025-09-24 11:05:40 +0200

    add record for creating specific migration history tables

---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared.Database/Models/MigrationsHistory.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using ViixsDockerManager.Shared.Helpers;
+
+namespace ViixsDockerManager.Shared.Database.Models;
+
+public record MigrationsHistory(string tableName)
+{
+    private const string MigrationsHistoryTableSuffix = "MigrationsHistory";
+    private const string DoubleUnderscore = "__";
+    private const char Underscore = '_';
+    public string GetMigrationTableName()
+    {
+        var migrationsTable = Guard.ValueIsNotNullOrEmpty(tableName, nameof(tableName));
+
+        migrationsTable = migrationsTable.TrimStart(Underscore);
+
+        if (migrationsTable.EndsWith(MigrationsHistoryTableSuffix, StringComparison.OrdinalIgnoreCase))
+        {
+            migrationsTable = migrationsTable[..^MigrationsHistoryTableSuffix.Length];
+        }
+
+        return $"{DoubleUnderscore}{migrationsTable}{MigrationsHistoryTableSuffix}";        
+    }
+}
