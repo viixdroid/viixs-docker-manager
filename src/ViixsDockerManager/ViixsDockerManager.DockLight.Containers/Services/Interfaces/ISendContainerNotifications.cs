818d8a03b59c4189c98dff32508a72c780fd9c75
Ravi Rampersad <viixdroid@gmail.com>
2025-09-24 11:02:28 +0200

    fixes

--- src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/Interfaces/ISendContainerNotifications.cs
+++ src/ViixsDockerManager/ViixsDockerManager.DockLight.Containers/Services/Interfaces/ISendContainerNotifications.cs
@@ -1,9 +1,10 @@
 ﻿using ViixsDockerManager.DockLight.Models.Commands;
+using ViixsDockerManager.Mediator;
 using ViixsDockerManager.Shared.Services;
 
 namespace ViixsDockerManager.DockLight.Services.Interfaces;
 
-internal interface ISendContainerNotifications : IViixsBaseService
+internal interface ISendContainerNotifications : IViixsBaseService, ICommandNotificationSender
 {
     Task SendContainerKilled(BaseContainerActionCommand containerActionCommand);
     Task SendContainerRestarted(BaseContainerActionCommand containerActionCommand);
