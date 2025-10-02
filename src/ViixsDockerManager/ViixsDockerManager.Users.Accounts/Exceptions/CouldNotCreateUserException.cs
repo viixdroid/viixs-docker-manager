33a90482b2b013705d527649bffd3f2b51d10c97
Ravi Rampersad <ravi.rampersad@conclusion.nl>
2025-10-02 17:56:15 +0200

    for now use an excpetion when creating a use goes wrong (we should use websockets for communication of what went wrong)

---
+++ src/ViixsDockerManager/ViixsDockerManager.Users.Accounts/Exceptions/CouldNotCreateUserException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+using ViixsDockerManager.Shared.Exceptions;
+
+namespace ViixsDockerManager.Users.Accounts.Exceptions;
+
+public class CouldNotCreateUserException(string createUserErrors)
+    : ViixsDockerManagerWithHttpStatusCodeException($"Could not create user account: {createUserErrors}", HttpStatusCode.BadRequest)
+{
+}
