3bfadc4987656436091a944adcb6415554d23c0d
Ravi Rampersad <ravi.rampersad@conclusion.nl>
2025-10-02 17:55:03 +0200

    fixes in the path for these uesers

--- src/ViixsDockerManager/viixsdockermanager.client/src/services/DockLightEnvironmentService.ts
+++ src/ViixsDockerManager/viixsdockermanager.client/src/services/DockLightEnvironmentService.ts
@@ -1,5 +1,5 @@
 import type { DockLightEnvironment } from '../models/dock-light-environment'
-import type { CreateDockLightEnvironmentCommand } from '../pages/setup/(models)/initialdocklightenvironment'
+import type { CreateDockLightEnvironmentCommand } from '../pages/setup/_models/initialdocklightenvironment'
 import backendApi from '../clients/BackendApiClient'
 
 const Url = 'docklightenvironments'
