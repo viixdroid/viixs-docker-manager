4a8d8b92de502db62eac9501030c567ded0a45be
Ravi Rampersad <ravi.rampersad@conclusion.nl>
2025-10-16 08:08:33 +0200

    Create interface for allowing multiple implementations to request to create user accounts

---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared.Models/Commands/Users/ICreateUserAccountCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace ViixsDockerManager.Shared.Models.Commands.Users;
+
+public interface ICreateUserAccountCommand
+{
+    string EmailAddress { get; }
+    string Password { get; }
+    string Role { get; }
+}
--- src/ViixsDockerManager/ViixsDockerManager.Shared.Services/ISharedUserAccountService.cs
+++ src/ViixsDockerManager/ViixsDockerManager.Shared.Services/ISharedUserAccountService.cs
@@ -4,5 +4,5 @@ namespace ViixsDockerManager.Shared.Services;
 
 public interface ISharedUserAccountService
 {
-    Task CreateUserAccount(CreateUserAccountCommand createUserAccountCommand, CancellationToken cancellationToken = default);
+    Task CreateUserAccount(ICreateUserAccountCommand createUserAccountCommand, CancellationToken cancellationToken = default);
 }
