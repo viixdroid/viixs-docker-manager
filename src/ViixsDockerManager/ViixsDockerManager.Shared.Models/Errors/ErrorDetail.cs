7a99ff3af5a5913cf9b1d4e2cc4cd67f18d3cd9c
Ravi Rampersad <ravi.rampersad@conclusion.nl>
2025-10-17 17:26:40 +0200

    Add error detail class and a map of errors

---
+++ src/ViixsDockerManager/ViixsDockerManager.Shared.Models/Errors/ErrorDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace ViixsDockerManager.Shared.Models.Errors;
+
+/// <summary>
+/// Represents an error with its code, description, and category.
+/// </summary>
+/// <param name="Code"></param>
+/// <param name="Description"></param>
+/// <param name="Category"></param>
+public sealed record ErrorDetail(string Code, string Description, string Category);
