46b8bbcb6b6584cd7213f0e13e5992b92044dccf
Ravi Rampersad <ravi.rampersad@conclusion.nl>
2025-10-17 17:40:17 +0200

    move exception class so it can be used  in multiple projects

--- src/ViixsDockerManager/ViixsDockerManager.Users.Accounts/Exceptions/CouldNotCreateUserException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-using ViixsDockerManager.Shared.Exceptions;
-
-namespace ViixsDockerManager.Users.Accounts.Exceptions;
-
-public class CouldNotCreateUserException(string createUserErrors)
-    : ViixsDockerManagerWithHttpStatusCodeException($"Could not create user account: {createUserErrors}", HttpStatusCode.BadRequest)
-{
-}
