d49bac924a8aff4ab1593b1347130ab9e5b7a82e
Ravi Rampersad <viixdroid@gmail.com>
2025-10-30 18:32:57 +0100

    added a Test Attribute

---
+++ src/ViixsDockerManager/ViixsDockerManager.Mediator/ConTestAttribute.cs
@@ -0,0 +1,25 @@
+﻿using ViixsDockerManager.Mediator.Commands;
+using ViixsDockerManager.Mediator.Queries;
+
+namespace ViixsDockerManager.Mediator;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class ConTestAttribute<TConType> : Attribute
+    where TConType : class
+{
+    public ConTestAttribute()
+    {
+        var conTypeType = typeof(TConType);
+        if (!typeof(IQuery).IsAssignableFrom(conTypeType)
+            && !typeof(ICommand).IsAssignableFrom(conTypeType))
+        {
+            throw new ArgumentException("TConType must implement either IQuery or ICommand interface.");
+        }
+        ConTypeName = conTypeType.Name;
+    }
+
+    public string ConTypeName { get; }
+    public string ControllerName { get; init; } = string.Empty;
+    public string Action { get; init; } = string.Empty;
+    public HttpMethod HttpMethod { get; init; } = HttpMethod.Get;
+}
