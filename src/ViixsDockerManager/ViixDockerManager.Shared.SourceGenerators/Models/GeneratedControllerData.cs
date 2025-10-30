e1135440d7e554fb8250f7a4e2e4f017d7fd467d
Ravi Rampersad <viixdroid@gmail.com>
2025-10-30 18:32:16 +0100

    Add source generator for controller automation
    Introduce a new source generator to automate the creation of
    controller classes in the ViixDockerManager project. This includes
    adding `ViixControllerAttributeDefinition.cs` for attribute
    constants, `GeneratedControllerData.cs` for metadata, and
    `ViixControllerSourceGenerator.cs` for the generator logic using
    Roslyn. Update the project and solution files to include the new
    source generator project, enhancing maintainability and reducing
    manual coding effort.

---
+++ src/ViixsDockerManager/ViixDockerManager.Shared.SourceGenerators/Models/GeneratedControllerData.cs
@@ -0,0 +1,24 @@
+﻿namespace ViixDockerManager.Shared.SourceGenerators.Models;
+
+public readonly record struct GeneratedControllerData
+{
+    public readonly string Action;
+    public readonly string TargetClassName;
+    public readonly string FullTargetTypeName;
+    public readonly string GeneratedClassName;
+    public readonly string GeneratedMethodName;
+    public readonly string ParameterType;
+    public readonly string? ReturnType;
+
+    public GeneratedControllerData(string action, string targetClassName, string fullTargetTypeName, string generatedClassName, string generatedMethodName, string parameterType, string? returnType)
+    {
+        Action = action;
+        TargetClassName = targetClassName;
+        FullTargetTypeName = fullTargetTypeName;
+        GeneratedClassName = generatedClassName;
+        GeneratedMethodName = generatedMethodName;
+        ParameterType = parameterType;
+        ReturnType = returnType;
+    }
+
+}
